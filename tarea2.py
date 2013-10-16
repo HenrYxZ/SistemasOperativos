decf6c6d4d856e5599d5ec9b9460edf512c9f9bb
HenrY <hjhenriq@gmail.com>
2013-10-16 18:53:18 -0300

    no terminado

---
+++ tarea2.py
@@ -0,0 +1,11 @@
+# Tarea 2
+# C - Look Simulation
+beginning = 0
+end = 100
+text = input("ingrese lista de peticiones")
+petitions = text.split("-")
+first = petitions[0]
+del petitions[0]
+while(petitions):
+	for cilinder in petitions:
+		distance = 
