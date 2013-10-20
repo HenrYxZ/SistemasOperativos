209e8a01f9e12a4fd0afac2042e3f66116e489f9
HenrY <hjhenriq@gmail.com>
2013-10-20 18:44:15 -0300

    if no funciona

--- tarea2.py
+++ tarea2.py
@@ -3,12 +3,11 @@
 beginning = 0
 end = 100
 
-text = raw_input("ingrese lista de peticiones")
-current = input("ingrese la posicion inicial del cabezal")
-
+text = raw_input("ingrese lista de peticiones	")
+current = input("ingrese la posicion inicial del cabezal	")
+print current
 petitions = text.split("-")
 petitions.sort()
-print petitions
 order = []
 times = []
 total_time = 0
@@ -16,18 +15,24 @@ total_time = 0
 # OJO: ------ Se maneja la lista petitions como strings de numeros y no ints
 
 while(petitions):
+
 	# Primero se encuentra el mas cercano
 	min_distance = 100
-	min_cilinder = 0
+	min_cilinder = petitions[0]
+	print petitions
 	for cilinder in petitions:
 		# Solo se cuentan cilindros mayores, pues estamos en orden creciente
+		print "hello"
 		num_cilinder = int(cilinder)
-		if num_cilinder < current:
-			break;
-		distance = num_cilinder - current
-		if distance < min_distance:
-			min_distance = distance
-			min_cilinder = cilinder
+		print str(num_cilinder) + "num_cilinder"
+		print str(current) + "current"
+		# ************ PROBLEMAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA ********************
+		if num_cilinder > current:
+			distance = num_cilinder - current
+			print distance
+			if distance < min_distance:
+				min_distance = distance
+				min_cilinder = cilinder
 
 	# Luego este se guarda junto con toda la informacion
 	current = min_cilinder
@@ -35,19 +40,21 @@ while(petitions):
 	times.append(min_distance)
 	total_time += min_distance
 
-	petitions.remove(current)
+	print current
+	petitions.remove(str(current))
 
 	# Si se llego al ultimo cilindro volver al inicio
-	if current > max(petitions):
+	if (len(petitions)!= 0) and (current > max(petitions)):
 		distance = current - int(petitions[0])
 
 		# Si esta ordenado el primero de la lista es el cilindro inicial
-		current = petitions[0]
+		current = int(petitions[0])
 		order.append(current)
 		times.append(distance)
 		total_time += distance
 
 		petitions.remove(current)
+		print current + " from C"
 
 	
 		
